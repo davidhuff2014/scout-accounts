6bef7f03a59cfe2a7ba3d29126f3c2922f624de3
davidhuff2014 <David.Huff@computer-critters.com>
2014-10-16 17:48:24 -0400

    troops GET #show spec

--- spec/controllers/troops_controller_spec.rb
+++ spec/controllers/troops_controller_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe TroopsController, :type => :controller do
     end
   end
 
+  describe 'GET #show' do
+    it 'sets @troop' do
+      troop = Fabricate(:troop)
+      get :show, id: troop.id
+      expect(assigns(:troop)).to eq(troop)
+    end
+
+    it 'renders the #show view' do
+      troop = Fabricate(:troop)
+      get :show, id: troop.id
+      expect(response).to render_template :show
+    end
+  end
+
 end
