55f33602bcf607feda9a4f127b77a5c82f7fd1e7
Dave Huff <David.Huff@computer-critters.com>
2015-01-12 16:55:48 -0500

    already have macro dried spec file

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -3,17 +3,8 @@ require 'rails_helper'
 feature 'user signs in', { js: true }  do
 
   scenario 'with valid email and password' do
-    # alice = Fabricate(:user)
-    # sign_in(alice)
     user = Fabricate(:user)
-    visit sign_in_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    # save_and_open_page
-    # binding.pry
-    click_button 'Sign In'
-    # binding.pry
+    sign_in(user)
     expect(page).to have_content user.full_name
-    # expect(page).to have_content alice.full_name
   end
 end
