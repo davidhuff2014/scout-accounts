4e001b7dec6d927bfbc424b3508e5195151a4eef
Dave Huff <David.Huff@computer-critters.com>
2015-01-14 19:49:34 -0500

    featuer user adds troop but am getting duplicates in my test

--- app/models/troop.rb
+++ app/models/troop.rb
@@ -1,3 +1,4 @@
 class Troop < ActiveRecord::Base
   has_many :scouts
+#TODO:  validates_uniqueness_of :charter_sponsor
 end
---
+++ spec/features/user_adds_troop_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'user adds troop' do
+
+  scenario 'valid user adds scout', { js: true } do
+    sign_in
+
+    visit new_troop_path
+    troop = Fabricate(:troop)
+    fill_in 'Troop number', with: troop.troop_number
+    fill_in 'Location', with: troop.location
+    fill_in 'Charter sponsor', with: troop.charter_sponsor
+    select '1938', from: "troop[inception_year]"
+    click_button 'Add Troop'
+    expect(page).to have_content(troop.troop_number)
+    expect(page).to have_content(troop.location)
+    expect(page).to have_content(troop.charter_sponsor)
+  end
+end
